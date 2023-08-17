ea707d0dd610ff9182f21d646cd55ac005a0e70d
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2023-08-17 16:33:16 -0400

    Fix

--- python_src/heisenberg_1d.py
+++ python_src/heisenberg_1d.py
@@ -22,12 +22,12 @@ def prepare_init_state(N):
     for i in range(0, N, 2):
         create_singlet(i, i+1)
 
-def create_u2_circuit(N, layers, H):
+def create_u2_circuit(N, num_blocks, H):
     # total params = 2Nl
     def circuit_2qubits(params):
         prepare_init_state(N)
         k = 0
-        for l in range(layers):
+        for l in range(num_blocks):
             for i in range(0, N, 2):
                 Spin_2(params[k], wires=[i, (i + 1) % N])
                 k += 1
@@ -45,12 +45,12 @@ def create_u2_circuit(N, layers, H):
     return circuit_2qubits
 
 
-def create_u3_circuit(N, layers, H):
+def create_u3_circuit(N, num_blocks, H):
     # total params = 4Nl
     def circuit_3qubits(params):
         prepare_init_state(N)
         k = 0
-        for l in range(layers):
+        for l in range(num_blocks):
             for i in range(0, N):
                 Spin_3(params[k], params[k + 1], params[k + 2], params[k+3], wires=[i, (i + 1) % N, (i + 2) % N])
                 k += 4
@@ -64,12 +64,12 @@ if __name__ == "__main__":
     J1 = 1.0
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_qubits', type=int, required=True)
+    parser.add_argument('--num-qubits', type=int, required=True, dest="num_qubits")
     parser.add_argument('--gate', type=int, required=True)
-    parser.add_argument('--num_layers', type=int, required=True)
-    parser.add_argument('--init_scale', type=float, default=1.0)
-    parser.add_argument('--learning_rate', type=float, default=5e-3)
-    parser.add_argument('--j2', type=float)
+    parser.add_argument('--num-blocks', type=int, required=True, dest="num_blocks")
+    parser.add_argument('--j2', type=float, required=True)
+    parser.add_argument('--init-scale', type=float, default=1.0, dest="init_scale")
+    parser.add_argument('--learning-rate', type=float, default=5e-3, dest="learning_rate")
 
     args = parser.parse_args()
     args = vars(args)
@@ -83,19 +83,19 @@ if __name__ == "__main__":
     dev = qml.device("lightning.qubit", wires=N)
 
     adam_step = args["learning_rate"]
-    layers = args["num_layers"]
+    num_blocks = args["num_blocks"]
     init_scale = args["init_scale"]
     epochs = 2000
     opt = qml.AdamOptimizer(stepsize=adam_step)
 
     if args["gate"] == 2:
-        circuit = qml.QNode(create_u2_circuit(N, layers, ham_sparse), dev, diff_method="adjoint")
-        init = init_scale*math.pi/(2*N*layers)
-        params = init * pnp.random.rand(2 * N * layers)
+        circuit = qml.QNode(create_u2_circuit(N, num_blocks, ham_sparse), dev, diff_method="adjoint")
+        init = init_scale*math.pi/(2*N*num_blocks)
+        params = init * pnp.random.rand(2 * N * num_blocks)
     elif args["gate"] == 3:
-        circuit = qml.QNode(create_u3_circuit(N, layers, ham_sparse), dev, diff_method="adjoint")
-        init = init_scale*math.pi/(4*N*layers)
-        params = init * pnp.random.rand(4 * N * layers)
+        circuit = qml.QNode(create_u3_circuit(N, num_blocks, ham_sparse), dev, diff_method="adjoint")
+        init = init_scale*math.pi/(4*N*num_blocks)
+        params = init * pnp.random.rand(4 * N * num_blocks)
     else:
         raise ValueError("spin gate size not supported")
 
